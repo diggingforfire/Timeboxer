30afb1d38e54ebc2aa537bb701064816c998d258
Robbie Luijben <diggingforfire@gmail.com>
2017-04-09 21:14:17 +0200

    Styling and colors

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2,12 +2,19 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent, Qt::Window | Qt::FramelessWindowHint),
+    QMainWindow(parent, Qt::Window | Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     ui->stopButton->setEnabled(false);
     ui->decreaseTimerButton->setEnabled(false);
+
+    setStyleSheet("QMainWindow { background: rgb(52, 163, 215); }");
+    ui->clock->setStyleSheet("QLabel { color: white; }");
+    ui->startButton->setStyleSheet("QPushButton { color: white; background: rgb(195, 36, 81); } QPushButton:disabled { background: rgb(219, 123, 150); }");
+    ui->stopButton->setStyleSheet("QPushButton { color: white; background: rgb(195, 36, 81); } QPushButton:disabled { background: rgb(219, 123, 150); }");
+    ui->increaseTimerButton->setStyleSheet("QPushButton { color: black; background: rgb(251, 227, 79); } QPushButton:disabled { background: rgb(249, 244, 164); }");
+    ui->decreaseTimerButton->setStyleSheet("QPushButton { color: black; background: rgb(239, 105, 38); } QPushButton:disabled { background: rgb(237, 166, 123); }");
 
     connect(ui->startButton, SIGNAL(released()), this, SLOT(start()));
     connect(ui->stopButton, SIGNAL(released()), this, SLOT(stop()));
@@ -55,13 +62,13 @@ void MainWindow::update()
     }
     else
     {
-        ui->clock->setStyleSheet("QLabel { color: red; }");
+        ui->clock->setStyleSheet("QLabel { color: rgb(52, 163, 215); }");
 
-        QTime dieTime= QTime::currentTime().addSecs(7);
+        QTime dieTime= QTime::currentTime().addSecs(5);
         while (QTime::currentTime() < dieTime)
             QCoreApplication::processEvents(QEventLoop::AllEvents, 100);
 
-        ui->clock->setStyleSheet("QLabel { color: black; }");
+        ui->clock->setStyleSheet("QLabel { color: white; }");
 
         stop();
     }
@@ -100,7 +107,7 @@ void MainWindow::increaseTimer()
 
 void MainWindow::setTimeLeftText(int minutes, int seconds)
 {
-    QString labelText = QString("%1:%2").arg(QString::number(minutes), QString::number(seconds).rightJustified(2, '0'));
+    QString labelText = QString("%1:%2").arg(QString::number(minutes).rightJustified(2, '0'), QString::number(seconds).rightJustified(2, '0'));
     ui->clock->setText(labelText);
 }
 
